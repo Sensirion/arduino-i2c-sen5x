d304edf144fd171b485ec020b738dff3db8fb6f6
Bjoern Muntwyler <Bjoern.Muntwyler@sensirion.com>
2022-01-04 15:58:28 +0100

    Adjust example usage to use setTemperatureOffsetSimple

--- examples/exampleUsage/exampleUsage.ino
+++ examples/exampleUsage/exampleUsage.ino
@@ -140,15 +140,33 @@ void setup() {
     printModuleVersions();
 #endif
 
-    // set RHT acceleration mode
-    //  0: Default / Air Purifier / IAQ (slow)
-    //  1: IAQ (fast)
-    //  2: IAQ (medium)
-    error = sen5x.setRhtAccelerationMode(0);
+    // set a temperature offset in degrees celsius
+    // By default, the temperature and humidity outputs from the sensor
+    // are compensated for the modules self-heating. If the module is
+    // designed into a device, the temperature compensation might need
+    // to be adapted to incorporate the change in thermal coupling and
+    // self-heating of other device components.
+    //
+    // A guide to achieve optimal performance, including references
+    // to mechanical design-in examples can be found in the app note
+    // “SEN5x – Temperature Compensation Instruction” at www.sensirion.com.
+    // Please refer to those application notes for further information
+    // on the advanced compensation settings used
+    // in `setTemperatureOffsetParameters`, `setWarmStartParameter` and
+    // `setRhtAccelerationMode`.
+    //
+    // Adjust tempOffset to account for additional temperature offsets
+    // exceeding the SEN module's self heating.
+    float tempOffset = 0.0;
+    error = sen5x.setTemperatureOffsetSimple(tempOffset);
     if (error) {
-        Serial.print("Error trying to execute setRhtAccelerationMode(): ");
+        Serial.print("Error trying to execute setTemperatureOffsetSimple(): ");
         errorToString(error, errorMessage, 256);
         Serial.println(errorMessage);
+    } else {
+        Serial.print("Temperature Offset set to ");
+        Serial.print(tempOffset);
+        Serial.println(" deg. Celsius");
     }
 
     // Start Measurement
@@ -209,49 +227,9 @@ void loop() {
         Serial.print("\t");
         Serial.print("NoxIndex:");
         if (isnan(noxIndex)) {
-            Serial.print("n/a");
+            Serial.println("n/a");
         } else {
-            Serial.print(noxIndex);
+            Serial.println(noxIndex);
         }
-        Serial.print("\t");
-    }
-
-    float numberConcentrationPm0p5;
-    float numberConcentrationPm1p0;
-    float numberConcentrationPm2p5;
-    float numberConcentrationPm4p0;
-    float numberConcentrationPm10p0;
-    float typicalParticleSize;
-
-    error = sen5x.readMeasuredPmValues(
-        massConcentrationPm1p0, massConcentrationPm2p5, massConcentrationPm4p0,
-        massConcentrationPm10p0, numberConcentrationPm0p5,
-        numberConcentrationPm1p0, numberConcentrationPm2p5,
-        numberConcentrationPm4p0, numberConcentrationPm10p0,
-        typicalParticleSize);
-
-    if (error) {
-        Serial.println("");
-        Serial.print("Error trying to execute readMeasuredPmValues(): ");
-        errorToString(error, errorMessage, 256);
-        Serial.println(errorMessage);
-    } else {
-        Serial.print("NumberConcentrationPm0p5:");
-        Serial.print(numberConcentrationPm0p5);
-        Serial.print("\t");
-        Serial.print("NumberConcentrationPm1p0:");
-        Serial.print(numberConcentrationPm1p0);
-        Serial.print("\t");
-        Serial.print("NumberConcentrationPm2p5:");
-        Serial.print(numberConcentrationPm2p5);
-        Serial.print("\t");
-        Serial.print("NumberConcentrationPm4p0:");
-        Serial.print(numberConcentrationPm4p0);
-        Serial.print("\t");
-        Serial.print("NumberConcentrationPm10p0:");
-        Serial.print(numberConcentrationPm10p0);
-        Serial.print("\t");
-        Serial.print("TypicalParticleSize:");
-        Serial.println(typicalParticleSize);
     }
 }
