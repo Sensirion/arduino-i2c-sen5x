b39b74c5017b70067ddbab7858dd346c1d9b4b48
lfierz <leonie.fierz@sensirion.com>
2023-01-19 15:20:33 +0100

    Fix comment for i2cBus parameter

--- src/SensirionI2CSen5x.h
+++ src/SensirionI2CSen5x.h
@@ -50,7 +50,7 @@ class SensirionI2CSen5x {
     /**
      * begin() - Initializes the SensirionI2CSen5x class.
      *
-     * @param serial Arduino stream object to be communicated with.
+     * @param i2cBus Arduino stream object to use for communication.
      *
      */
     void begin(TwoWire& i2cBus);
